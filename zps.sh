8216ac8d82fbda953fafb2008f41153f44ba3727
Salvatore <Stakct@users.noreply.github.com>
2014-10-21 04:15:24 +0200

    .

--- zps.sh
+++ zps.sh
@@ -40,12 +40,5 @@ elif [ $1 = "module-list" ]
 then
   php zftool.phar modules
 else
-  echo "Welcome to Zend Project Starter v0.1\n
-I'm here to help you to create a zen project with composer!\n
-\n
-Create project - Usage: sh zps.sh create DIRNAME\n
-\n
-Module list - Usage(inside project folder): sh zps.sh module-list\n
-\n
-Have fun! - Salvatore Tarda";
+  echo "Welcome to Zend Project Starter v0.1\nI'm here to help you to create a zen project with composer!\n\nCreate project - Usage: sh zps.sh create DIRNAME\n\nModule list - Usage(inside project folder): sh zps.sh module-list\n\nHave fun! - Salvatore Tarda";
 fi
